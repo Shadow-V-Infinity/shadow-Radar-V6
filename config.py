82b720e972edb24c353e53cadb9a6d7e09ada25d
Shadow-V-Infinity <270384599+Shadow-V-Infinity@users.noreply.github.com>
2026-04-15 03:51:10 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py (exemple, à adapter avec tes clés réelle>
+ODDS_API_KEY = "ta_clé_odds_api"
+ODDS_API_IO_KEY = "ta_clé_odds_api_io"
+FOOTBALL_DATA_KEY = "ta_clé_football_data"
+TELEGRAM_BOT_TOKEN = "ton_token_telegram"
+WEATHER_API_KEY = "ta_clé_api_météo"
