39ffdff736bd1d2896afb6840cae404c558a8805
Shadow-V-Infinity <270384599+Shadow-V-Infinity@users.noreply.github.com>
2026-04-15 14:47:47 +0200

    Delete config (3).py

--- config (3).py
+++
@@ -1,8 +0,0 @@
-# config.py (exemple, à adapter avec tes clés réelle>
-ODDS_API_KEY = "ta_clé_odds_api"
-ODDS_API_IO_KEY = "ta_clé_odds_api_io"
-FOOTBALL_DATA_KEY = "ta_clé_football_data"
-TELEGRAM_BOT_TOKEN = "ton_token_telegram"
-WEATHER_API_KEY = "ta_clé_api_météo"
-TENNIS_API_KEY = "ta_clé_api_tennis.com"
-BALLDONTLIE_API_KEY = "ta_clé_api_balldontlie"
